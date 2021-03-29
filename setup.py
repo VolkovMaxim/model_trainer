ab2dbe7b3f941beddad3e2402cca6f97ab8d7d5f
wolk1612 <mawolk1612@yandex.ru>
2021-03-29 14:40:53 +0300

    try not read from requariments

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
 from setuptools import find_packages, setup
-import os
-
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-
-def read_requirements():
-    """parses requirements from requirements.txt"""
-    reqs_path = os.path.join(__location__, 'requirements.txt')
-    with open(reqs_path, encoding='utf8') as f:
-        reqs = [line.strip() for line in f if not line.strip().startswith('#')]
-
-    names = []
-    links = []
-    for req in reqs:
-        if '://' in req:
-            links.append(req)
-        else:
-            names.append(req)
-    return {'install_requires': names, 'dependency_links': links}
 
 
 setup(name="model_trainer",
@@ -25,5 +6,6 @@ setup(name="model_trainer",
       vesion="0.1.0",
       description="True validation",
       author="wolk1612",
-      **read_requirements()
+      install_requires=["tqdm", "IPython", "collections", "torch",
+                        "copy", "numpy", "scipy", "matplotlib", "abc"]
       )
